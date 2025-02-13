04833f355690dafd2cdbfa95f38fb3bc6372816b
Boris Isac <boris.isac@webtech87.pt>
2025-02-13 13:27:34 +0000

    Delete psicologia directory

--- psicologia/books/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-class BooksAdmin(admin.ModelAdmin):
-    class Meta:
-        model=Books
-        list_display=['title', 'author', 'category', 'price', 'stock']
-
-admin.site.register(Books, BooksAdmin)
--- psicologia/main/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, HttpResponse
-from users.models import * 
-from django.contrib.auth.hashers import make_password, check_password
-# Create your views here.
-
-def index(request):
-    return render(request, 'index.html')

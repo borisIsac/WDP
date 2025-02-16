19df426e2164cb23674f625fc7669ea2c283c64d
= <=>
2025-02-16 16:54:48 +0000

    [UPDATE] gitignor has been added a secret_files and has been added on settings ALLOWED_HOSTS

--- psycology/psycology/settings.py
+++ psycology/psycology/settings.py
@@ -16,7 +16,8 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
 
 #SITE_DOMAIN = insert a domain here
 SITE_DOMAIN = 'http://localhost:8000'

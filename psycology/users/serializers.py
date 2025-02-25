e11ba065545dd4852ac7af25737253a8a417ce26
BorisIsac <boris.isac@webtech87.pt>
2025-02-25 17:35:23 +0000

    [UPDATE] sign_up and update personal datat is done

--- psycology/users/serializers.py
+++ psycology/users/serializers.py
@@ -1,6 +1,58 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from .models import NoteToken, CustomUser
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields = [
+            "id", 
+            "email", 
+            "full_name", 
+            "phone", 
+            "username",
+            "birthday", 
+            "gender", 
+            "country", 
+            "password"
+            ]
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
+
+    def create(self, validated_data):
+        '''
+        create new user instance, given the validated data
+        '''     
+        username = validated_data.get('email', validated_data['email'])
+        print(username)
+
+        
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            username=username, 
+            password=validated_data['password'],
+            full_name=validated_data['full_name'],
+            phone=validated_data['phone'],
+            gender=validated_data['gender'],
+            country=validated_data['country'],
+            birthday=validated_data['birthday'],
+        )
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        for attr, value in validated_data.items():
+            print('!!!!',attr, value)
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -17,23 +69,3 @@ class NoteTokenSerializer(serializers.ModelSerializer):
         fields = ["id", "description", "owner"]
 
     
-class CustomUserRegistrationSerializer(serializers.ModelSerializer):
-    
-    class Meta:   
-        model=CustomUser
-        fields = ['email', 'full_name', 'phone', 'gender', 'country', 'birthday', "username", 'password']
-
-    def create(self, validated_data):
-        user = CustomUser(
-            email = validated_data['email'],
-            full_name = validated_data['full_name'],     
-            phone = validated_data['phone'], 
-            gender = validated_data['gender'], 
-            country = validated_data['country'], 
-            birthday = validated_data['birthday'], 
-            username = validated_data['username'], 
-        )
-
-        user.set_password(validated_data['password'])
-        user.save()
-        return user

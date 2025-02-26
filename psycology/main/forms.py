a098632254e2f5cde49a139e6d1be2f31e46b8d1
Felipe Silva <felipe.silva@webtech87.pt>
2025-02-26 17:46:51 +0000

    [FIX] Insert logo, outer radius, padding and form white border

--- psycology/main/forms.py
+++ psycology/main/forms.py
@@ -4,25 +4,28 @@ from django.db import models
 class ContactRequestForm(forms.Form):
     
     class Subjects(models.TextChoices):
-        SELECT = 'Select an option', 'Select an option'
-        BOOKS  = 'Books', 'Books'
-        COURSES = 'Courses', 'Courses'
-        MENTORSHIP = 'Mentorship', 'Mentorship'
-        IMMERSIONS = 'Immersions', 'Immersions'
+        SELECT = 'Assunto', 'Assunto'
+        BOOKS  = 'Livros', 'Livros'
+        COURSES = 'Cursos', 'Cursos'
+        MENTORSHIP = 'Mentorias', 'Mentorias'
+        IMMERSIONS = 'Imerções', 'Imerções'
 
     full_name = forms.CharField(
+        label='Nome:',
         max_length=100,
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Insert your full name'})
+        widget=forms.TextInput(attrs={'class': 'form-control white-border', 'placeholder': 'Insira o seu nome'})
         )
     email = forms.EmailField(
-        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Insert your email'})
+        widget=forms.EmailInput(attrs={'class': 'form-control white-border', 'placeholder': 'Insira o seu email'})
     )
     subject = forms.ChoiceField(
+        label='Assunto:',
         choices = Subjects.choices,
         required=True,
         initial= Subjects.SELECT,
-        widget=forms.Select(attrs={'class': 'form-control'})
+        widget=forms.Select(attrs={'class': 'form-control white-border'})
     )
     message = forms.CharField(
-        widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'cols': 40, 'placeholder': 'Write your message here'})
+        label='Mensagem:',
+        widget=forms.Textarea(attrs={'class': 'form-control white-border', 'rows': 5, 'cols': 40, 'placeholder': 'Escreva aqui a sua mensagem'})
     )

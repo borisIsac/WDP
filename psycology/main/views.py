70c11a0cc824794532e6e395831959d1b399142c
Felipe Silva <felipe.silva@webtech87.pt>
2025-02-27 14:55:22 +0000

    [UPDATE] new feat: send email with form information

--- psycology/main/views.py
+++ psycology/main/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from users.models import * 
 from django.contrib.auth.hashers import make_password, check_password
 from .forms import ContactRequestForm
+from django.core.mail import send_mail, EmailMultiAlternatives
+from django.template.loader import render_to_string
+from secret_files.secret_data import *
+import logging
+
 # Create your views here.
 
 def index(request):
@@ -22,14 +27,45 @@ def site_under_construction(request):
         }
         if form.is_valid():
             cd = form.cleaned_data
-            request.form.full_name = cd['full_name']
-            request.form.email = cd['email']
-            form.save()
-            return render(request, "page_under_construction.html", context)
+            full_name = cd['full_name']
+            email = cd['email']
+            subject = cd['subject']
+            message = cd['message']
+
+            email_subject = f'Novo Contacto Sobre: {subject}'
+
+            email_body = render_to_string('emails/contact_email.html', {
+                'full_name': full_name,
+                'email': email,
+                'subject': subject,
+                'message': message
+            })  
+            
+            logger = logging.getLogger(__name__)
+            
+            try:
+                email_msg = EmailMultiAlternatives(
+                    subject=email_subject,
+                    body=email_body,
+                    from_email=EMAIL_SENDER,
+                    to=EMAIL_SENDER,
+                    reply_to=[email]
+                )
+
+                email_msg.attach_alternative(email_body, "text/html")
+                email_msg.send(fail_silently=False)
+            
+                return render(request, "page_under_construction.html", context)
+
+            # Raise error if email not sent because fail_silently=False
+            except Exception as e:
+                logger.error(f"Email sending failed: {e}")
+                return render(request, "page_under_construction.html", context)
+
     else:
         form = ContactRequestForm()
         context = {
             'form': form,
-            'submit': 'Submit'
+            'submit': 'Enviar Mensagem'
         }
     return render(request, 'page_under_construction.html', context)

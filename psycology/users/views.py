3667c500f8c1fae7e9bbc7501300b790f818c2ad
BorisIsacWebTech87 <boris.isac@webtech87.pt>
2025-03-03 11:17:12 +0000

    [FIX] urls users, possibility to sign_up for not autentificated users is not allowed

--- psycology/users/views.py
+++ psycology/users/views.py
@@ -22,6 +22,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib.auth import get_user_model
 from rest_framework import generics
+from .permissions import *
 
 
 def activation_sended(request):
@@ -147,6 +148,7 @@ def is_authenticated(request):
 
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def logout(request):
     try:
         response = Response()
@@ -187,7 +189,7 @@ User = get_user_model()
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsNotAuthenticated, AllowAny]
 
 
 class UserProfileView(generics.RetrieveUpdateAPIView):

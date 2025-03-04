88086acac1b0be494d3bf54d3e83f600b85cbc0c
BorisIsacWebTech87 <boris.isac@webtech87.pt>
2025-03-04 12:43:00 +0000

    [ADD] comments to books. methods allowed [GET] -any one , [POST, PUT]-owners, [DELETE] - superuser

--- psycology/books/admin.py
+++ psycology/books/admin.py
@@ -5,4 +5,9 @@ class BooksAdmin(admin.ModelAdmin):
     model=Books
     list_display=['title', 'author', 'category', 'price', 'stock']
 
+class BooksComment(admin.ModelAdmin):
+    model = Comment
+    list_display=['user','book','published_at']
+
 admin.site.register(Books, BooksAdmin)
+admin.site.register(Comment, BooksComment)
--- psycology/books/views.py
+++ psycology/books/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
+from .permissions import *
 from rest_framework import generics
 from users.permissions import IsSuperuser
 from django.shortcuts import get_object_or_404
@@ -33,4 +34,38 @@ class BookViewSet(viewsets.ModelViewSet):
         Get a single book by primary key.
         """
         return generics.get_object_or_404(self.queryset, pk=self.kwargs["pk"])
+    
+
+class CommentsViewSet(viewsets.ModelViewSet):
+    '''
+    print all comments to each books.
+    return JSON
+    '''
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    
+    def get_permissions(self):
+        """
+        Assign different permissions based on the action.
+        """
+        if self.action in ["create", "update", "partial_update",]:
+            return [permissions.IsAuthenticated(), IsOwnerOrReadOnly()]
+        elif self.action in ["destroy"]:
+            return [permissions.IsAuthenticated(), IsSuperuser()]
+        return [permissions.AllowAny()]
+
+
+    
+    def get_queryset(self):
+        """
+        Get a comment witch bellongs to single book by primary key.
+        """
+        book_id = self.kwargs['book_id']
+    
+        return Comment.objects.filter(book_id=book_id)
+    
+    def perform_create(self, serializer):
+        book_id = self.kwargs['book_id']
+        return serializer.save(user=self.request.user, book_id=book_id)
     

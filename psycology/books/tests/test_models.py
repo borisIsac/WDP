84d262e2128f6c3a21de1374faf0557641e72e47
= <=>
2025-03-12 14:21:01 +0000

    [UPDATE]TDD structure tests

--- psycology/books/tests/test_models.py
+++ psycology/books/tests/test_models.py
@@ -6,8 +6,8 @@ from django_countries import countries
 
 
 class BooksTest(TestCase):
-    def test_create_new_book(self):
-        new_book = Books.objects.create(
+    def setUp(self):
+        self.book_1 = Books.objects.create(
             title="Docker для DevOps",
             author="Иван Иванов",
             description="Обзор Docker для разработчиков и DevOps.",
@@ -20,123 +20,102 @@ class BooksTest(TestCase):
             link_to_download="https://example.com/download"
         )
 
-        self.assertEqual(new_book.title,"Docker для DevOps") 
-        self.assertEqual(new_book.author, "Иван Иванов") 
-        self.assertEqual(new_book.description, "Обзор Docker для разработчиков и DevOps.") 
-        self.assertEqual(new_book.published_date, "2025-03-01") 
-        self.assertEqual(new_book.price, 29.99) 
-        self.assertEqual(new_book.format, Books.Format.PAPERBACK) 
-        self.assertEqual(new_book.stock, 100) 
-        self.assertEqual(new_book.category, "DevOps") 
-        self.assertEqual(new_book.link_to_download, "https://example.com/download") 
-        self.assertEqual(new_book.link_to_ebook, "https://example.com/ebook")
-        self.assertEqual(str(new_book), "Иван Иванов-Docker для DevOps")
-
-    def test_create_new_comments(self):
-        user = CustomUser.objects.create(
-                is_active=True,
-                email="testemail@email.com",
-                password="12345",
-                full_name="Test User Test",
-                phone="987654321",
-                username="TestUsernameTest",
-                birthday="2000-01-01",
-                gender=CustomUser.Gender.MALE,
-                country = countries._countries['PT']
-            )
-        
-        book = Books.objects.create(
-                title="Docker для DevOps",
-                author="Иван Иванов",
-                description="Обзор Docker для разработчиков и DevOps.",
-                published_date="2025-03-01",
-                price=29.99,
-                format=Books.Format.PAPERBACK,
-                stock=100,
-                category="DevOps",
-                link_to_ebook="https://example.com/ebook",
-                link_to_download="https://example.com/download"
-            )
-
-
-        new_comment = Comment.objects.create(
-            book = book,
-            user = user,
-            text_comment = "!!!comment test 1!!!"
+        self.user_1 = CustomUser.objects.create_user(
+            is_active=True,
+            email="testemail3@email.com",
+            password="12345",
+            full_name="Test1 User Test",
+            phone="987654321",
+            username="TestUsernameTest",
+            birthday="2000-01-01",
+            gender=CustomUser.Gender.MALE,
+            country=countries._countries['PT']
         )
 
-        self.assertEqual(user.__str__(), 'Test User Test',)
-        self.assertEqual(book.__str__(),'Иван Иванов-Docker для DevOps')
-        self.assertEqual(new_comment.__str__(), "Test User Test-Иван Иванов-Docker для DevOps")
+        self.user_2 = CustomUser.objects.create_user(
+            is_active=True,
+            email="testemail2@email.com",
+            password="12345",
+            full_name="Test User Test",
+            phone="987654321",
+            username="TestUsernameTest",
+            birthday="2000-01-01",
+            gender=CustomUser.Gender.MALE,
+            country=countries._countries['PT']
+        )
+
+        self.book_2 = Books.objects.create(
+            title="Docker для DevOps",
+            author="Иван Иванов",
+            description="Обзор Docker для разработчиков и DevOps.",
+            published_date="2025-03-01",
+            price=29.99,
+            format=Books.Format.PAPERBACK,
+            stock=100,
+            category="DevOps",
+            link_to_ebook="https://example.com/ebook",
+            link_to_download="https://example.com/download"
+        )
+
+        self.comment_1 = Comment.objects.create(
+            book=self.book_1,
+            user=self.user_1,
+            text_comment="!!!comment test 1!!!"
+        )
+
+        self.r1 = BookRating.objects.create(
+            rating=2,
+            user_id=self.user_1.id,
+            book_id=self.book_1.id,
+        )
+        self.r2 = BookRating.objects.create(
+            rating=3,
+            user_id=self.user_1.id,
+            book_id=self.book_2.id,
+        )
+        self.r3 = BookRating.objects.create(
+            rating=5,
+            user_id=self.user_2.id,
+            book_id=self.book_1.id,
+        )
+
+
+    def test_create_new_book_title(self):
+        self.assertEqual(self.book_1.title,"Docker для DevOps")
+
+    def test_create_new_book_author(self):
+        self.assertEqual(self.book_1.author, "Иван Иванов")
+
+    def test_create_new_book_description(self):
+        self.assertEqual(self.book_1.description, "Обзор Docker для разработчиков и DevOps.")
+
+    def test_create_new_book_published_date(self):
+        self.assertEqual(self.book_1.published_date, "2025-03-01")
+
+    def test_create_new_book_price(self):
+        self.assertEqual(self.book_1.price, 29.99)
+
+    def test_create_new_book_format(self):
+        self.assertEqual(self.book_1.format, Books.Format.PAPERBACK)
+
+    def test_create_new_book_stock(self):
+        self.assertEqual(self.book_1.stock, 100)
+
+    def test_create_new_book_category(self):
+        self.assertEqual(self.book_1.category, "DevOps")
+
+    def test_create_new_book_download_link(self):
+        self.assertEqual(self.book_1.link_to_download, "https://example.com/download")
+
+    def test_create_new_book_ebook_link(self):
+        self.assertEqual(self.book_1.link_to_ebook, "https://example.com/ebook")
+
+    def test_create_new_book__str__(self):
+        self.assertEqual(str(self.book_1), "Иван Иванов-Docker для DevOps")
+
+    def test_create_new_comments_book(self):
+        self.assertEqual(self.comment_1.__str__(), "Test1 User Test-Иван Иванов-Docker для DevOps")
 
     def test_average_rating(self):
-
-            user = CustomUser.objects.create(
-                is_active=True,
-                email="testemail@email.com",
-                password="12345",
-                full_name="Test User Test",
-                phone="987654321",
-                username="TestUsernameTest",
-                birthday="2000-01-01",
-                gender=CustomUser.Gender.MALE,
-                country=countries._countries['PT']
-            )
-
-            user2 = CustomUser.objects.create(
-                is_active=True,
-                email="testemail1@email.com",
-                password="12345",
-                full_name="Test User Test",
-                phone="987654321",
-                username="TestUsernameTest",
-                birthday="2000-01-01",
-                gender=CustomUser.Gender.MALE,
-                country=countries._countries['PT']
-            )
-
-            book1 = Books.objects.create(
-                title="Docker для DevOps",
-                author="Иван Иванов",
-                description="Обзор Docker для разработчиков и DevOps.",
-                published_date="2025-03-01",
-                price=29.99,
-                format=Books.Format.PAPERBACK,
-                stock=100,
-                category="DevOps",
-                link_to_ebook="https://example.com/ebook",
-                link_to_download="https://example.com/download"
-            )
-            book2 = Books.objects.create(
-                title="Docker для DevOps",
-                author="Иван Иванов",
-                description="Обзор Docker для разработчиков и DevOps.",
-                published_date="2025-03-01",
-                price=29.99,
-                format=Books.Format.PAPERBACK,
-                stock=100,
-                category="DevOps",
-                link_to_ebook="https://example.com/ebook",
-                link_to_download="https://example.com/download"
-            )
-            r1 = BookRating(
-                rating=2,
-                user_id=1,
-                book_id=1,
-            )
-            r2 = BookRating(
-                rating=3,
-                user_id=1,
-                book_id=2,
-            )
-            r3 = BookRating(
-                rating=5,
-                user_id=2,
-                book_id=1,
-            )
-
-            r1.save()
-            r2.save()
-            r3.save()
-
-            self.assertEqual(book1.average_rating(), 3.5)
+        self.assertEqual(self.book_1.average_rating(), 3.5)
+        self.assertEqual(self.book_2.average_rating(), 3)
